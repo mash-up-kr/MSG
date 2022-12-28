30661edd6cf5de8f69783fe5cd816b1b3e3b4082
HaJunRyu <hjr4557@naver.com>
2022-12-28 18:51:39 +0900

    Env: reset css를 적용한다

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Global } from '@emotion/react';
+import { globalStyles } from '@/styles/globalStyles';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
