f4b83f837e60e1c366442e04b619b198829f4eec
HaJunRyu <hjr4557@naver.com>
2023-01-02 00:36:42 +0900

    Feat: theme.color 객체에 core와 team color를 추가한다
    core와 team color는 linear-gradient로 되어있어 background: ${theme.color.$teamname} 이런식으로 사용해야 합니다.

--- src/styles/theme/color.ts
+++ src/styles/theme/color.ts
@@ -4,6 +4,14 @@ export const color = {
   gray700: '#2B2F3C',
   gray500: '#495066',
   white: '#ffffff',
+  mashup: 'linear-gradient(98.54deg, #FF1C60 0%, #5243FF 100%)',
+  ios: 'linear-gradient(134.55deg, #4A527C 15.9%, #ADBAFF 92.94%)',
+  node: 'linear-gradient(154.94deg, #FF2F61 0.01%, #FFB48A 109.79%)',
+  branding: 'linear-gradient(141.58deg, #FFD702 3.14%, #FF7629 99.26%)',
+  design: 'linear-gradient(135deg, #6842FF 0%, #C446FF 100%)',
+  web: 'linear-gradient(111.03deg, #0A31FF 11.37%, #0AB5FF 93.31%)',
+  spring: 'linear-gradient(69.27deg, #1CFF77 -1.02%, #43D2FF 110.44%)',
+  android: 'linear-gradient(91.02deg, #0DA300 3.3%, #9EFF00 104.01%)',
 } as const;
 
 export type ColorType = typeof color;
