95bfaad491372d09e0fce32777888055c2ed99a3
HaJunRyu <hjr4557@naver.com>
2023-01-02 01:15:42 +0900

    Feat: 앱의 공통적인 Layout을 담당 할 Layout 컴포넌트를 정의한다

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import type { AppProps } from 'next/app';
 import { Global, ThemeProvider } from '@emotion/react';
 import { globalStyles } from '@/styles/globalStyles';
 import { theme } from '@/styles/theme';
+import { Layout } from '@/components/common';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Global styles={globalStyles} />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
     </>
   );
---
+++ src/components/common/Layout/Layout.styled.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Layout = styled.div`
+  width: 100%;
+  max-width: 76.8rem;
+  margin: 0 auto;
+`;
