432d56cbb07fa007a848eba37f83cfa0fe3c7bf8
HaJunRyu <hjr4557@naver.com>
2023-01-04 01:19:23 +0900

    Refactor: Layout display속성을 flex로 수정한다
    Layout 자식 요소에서 flex-grow를 사용을 위해 Layout 컴포넌트의 display속성을 수정해줍니다.

--- src/components/common/Layout/Layout.styled.ts
+++ src/components/common/Layout/Layout.styled.ts
@@ -1,7 +1,10 @@
 import styled from '@emotion/styled';
 
 export const Layout = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
   width: 100%;
   max-width: 76.8rem;
+  height: 100vh;
   margin: 0 auto;
 `;
