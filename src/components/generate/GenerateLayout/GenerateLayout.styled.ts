4eb6ee3da3fbab1e41a74d4f8ed120e1cb123c7a
HaJunRyu <hjr4557@naver.com>
2023-01-08 02:28:06 +0900

    Feat: Generate페이지의 Layout을 정의한다

---
+++ src/components/generate/GenerateLayout/GenerateLayout.styled.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const GenerateLayout = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  flex-grow: 1;
+  justify-content: space-between;
+  height: 100%;
+  max-height: 76rem;
+  padding: 0rem 0 6rem;
+`;
---
+++ src/components/generate/index.ts
@@ -0,0 +1,3 @@
+export { default as GenerateLayout } from './GenerateLayout/GenerateLayout.component';
+export { default as PreviewSection } from './PreviewSection/PreviewSection.component';
+export { default as ControlSection } from './ControlSection/ControlSection.component';
