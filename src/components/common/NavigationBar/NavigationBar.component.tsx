3ea3bbe29987cdf58734e534dab76b6980300262
HaJunRyu <hjr4557@naver.com>
2023-01-09 20:53:34 +0900

    Refactor: NavigationBar를 footer요소로 수정하고 옵션을 추가한다
    result페이지에서의 사용도 고려하여 backButton을 숨기고 rightButton의 color를 변경할 수 있도록 수정해주었습니다.

--- src/components/common/NavigationBar/NavigationBar.component.tsx
+++ src/components/common/NavigationBar/NavigationBar.component.tsx
@@ -1,11 +1,15 @@
+import { ColorType } from '@/styles/theme/color';
+import { KeyOf } from '@/types/helper';
 import { MouseEventHandler } from 'react';
 import * as Styled from './NavigationBar.styled';
 
 interface NavigationBarProps {
   rightButtonText: string;
   rightButtonEvent: MouseEventHandler<HTMLButtonElement>;
-  backButtonEvent: MouseEventHandler<HTMLButtonElement>;
+  backButtonEvent?: MouseEventHandler<HTMLButtonElement>;
   isDisabled?: boolean;
+  isHideBackButton?: boolean;
+  rightButtonColor?: KeyOf<ColorType>;
 }
 
 const NavigationBar = ({
@@ -13,15 +17,24 @@ const NavigationBar = ({
   rightButtonEvent,
   backButtonEvent,
   isDisabled = false,
+  isHideBackButton = false,
+  rightButtonColor = 'white',
 }: NavigationBarProps) => {
   return (
     <Styled.NavigationBar>
-      <Styled.BackButton type="button" onClick={backButtonEvent}>
-        <Styled.LessThanIcon />
-      </Styled.BackButton>
-      <Styled.RightButton type="button" onClick={rightButtonEvent} disabled={isDisabled}>
+      <Styled.RightButton
+        type="button"
+        onClick={rightButtonEvent}
+        disabled={isDisabled}
+        color={rightButtonColor}
+      >
         {rightButtonText}
       </Styled.RightButton>
+      {!isHideBackButton && (
+        <Styled.BackButton type="button" onClick={backButtonEvent}>
+          <Styled.LessThanIcon />
+        </Styled.BackButton>
+      )}
     </Styled.NavigationBar>
   );
 };
