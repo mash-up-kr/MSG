394c0aa16018afb4ef0a99fe96aea977053cdad5
Baek2back <dumbuk12@naver.com>
2023-01-13 02:23:51 +0900

    Refactor: Registration 페이지의 name에 대한 UI 변경사항을 반영한다

--- src/components/registration/NameForm/NameForm.styled.ts
+++ src/components/registration/NameForm/NameForm.styled.ts
@@ -1,6 +1,11 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
@@ -12,12 +17,14 @@ export const Label = styled.label`
   font-weight: 700;
   font-size: 2.8rem;
   line-height: 3.4rem;
+  white-space: pre-wrap;
   text-align: center;
 `;
 
 export const Input = styled.input`
   ${({ theme }) => css`
     position: relative;
+    width: 200px;
     padding: 2rem 2.4rem;
     color: ${theme.color.white};
     font-weight: 700;
@@ -48,5 +55,16 @@ export const Input = styled.input`
     &:focus::placeholder {
       color: transparent;
     }
+  `}
+`;
+
+export const Note = styled.p`
+  ${({ theme }) => css`
+    margin-top: 2rem;
+    color: ${theme.color.gray500};
+    font-weight: 400;
+    font-size: 1.4rem;
+    line-height: 1.7rem;
+    text-align: center;
   `}
 `;
