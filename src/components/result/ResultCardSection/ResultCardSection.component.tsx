1cd3fef261052e7aca8aae253dacd69eb8ddfe5a
Baek2back <dumbuk12@naver.com>
2023-01-15 22:39:58 +0900

    Feat: 링크 공유하기 기능을 추가한다

--- src/components/result/ResultCardSection/ResultCardSection.component.tsx
+++ src/components/result/ResultCardSection/ResultCardSection.component.tsx
@@ -3,6 +3,9 @@ import { ResultCard } from '@/components/result';
 import { Platform, PLATFORM_NAME_MAP } from '@/constants/platform';
 import { useRef } from 'react';
 import useDownloadElementToImage from '@/hooks/useDownloadElementToImage';
+import useWebShare from '@/hooks/useWebShare';
+import { copyToClipboard } from '@/utils/clipboard';
+import { ROUTES } from '@/constants/route';
 import * as Styled from './ResultCardSection.styled';
 
 interface ResultCardSectionProps {
@@ -14,6 +17,15 @@ const ResultCardSection = ({ platformName }: ResultCardSectionProps) => {
   const resultFileName = `mashong${PLATFORM_NAME_MAP[platformName]}.png`;
 
   const { handleSaveImage } = useDownloadElementToImage(resultCardRef, resultFileName);
+  const { isSupported, share } = useWebShare();
+
+  const handleShareLink = () => {
+    if (!isSupported) {
+      copyToClipboard(`${window.location.origin}${ROUTES.home}`);
+    } else {
+      share({ url: `${window.location.origin}${ROUTES.home}` });
+    }
+  };
 
   return (
     <Styled.ResultCardSection>
@@ -23,7 +35,7 @@ const ResultCardSection = ({ platformName }: ResultCardSectionProps) => {
         <Button size="l" isSelected={false} onClick={handleSaveImage}>
           카드 저장하기
         </Button>
-        <Button size="l" isSelected>
+        <Button size="l" isSelected onClick={handleShareLink}>
           공유하기
         </Button>
       </Styled.SaveAndShareContainer>
