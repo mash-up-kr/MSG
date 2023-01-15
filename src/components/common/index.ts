e94a93ce1b741edd0065a58de7dbbb4c3d80c319
HaJunRyu <hjr4557@naver.com>
2023-01-15 23:01:07 +0900

    Feat: Dialog 뒤 Deem영역인 Modal 컴포넌트를 정의한다

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,3 +4,5 @@ export { default as Button } from './Button/Button.component';
 export { default as LinearGradientSphere } from './LinearGradientSphere/LinearGradientSphere.component';
 export { default as Footer } from './Footer/Footer.component';
 export { default as BaseCard } from './BaseCard/BaseCard.component';
+export { default as Portal } from './Portal/Portal.component';
+export { default as Modal } from './Modal/Modal.component';
