589a5f860ce874d551fe33d460c9d5205e685045
Baek2back <dumbuk12@naver.com>
2023-01-15 22:39:10 +0900

    Feat: clipboard 유틸 함수를 정의한다

---
+++ src/utils/clipboard.ts
@@ -0,0 +1,36 @@
+import { detectOS } from './userAgent';
+
+const copyExecCommand = (text: string) => {
+  const isIOS = detectOS('iOS');
+  const textArea = document.createElement('textarea');
+
+  textArea.style.cssText = 'position: fixed; z-index: -1; opacity: 0';
+
+  textArea.contentEditable = 'true';
+  textArea.readOnly = true;
+
+  document.body.appendChild(textArea);
+  textArea.textContent = text;
+
+  if (isIOS) {
+    const selection = window.getSelection();
+    const range = document.createRange();
+
+    selection?.removeAllRanges();
+    selection?.addRange(range);
+
+    range.selectNodeContents(textArea);
+
+    textArea.setSelectionRange(0, textArea.value.length);
+  } else {
+    textArea.select();
+  }
+
+  document.execCommand('copy');
+
+  document.body.removeChild(textArea);
+};
+
+export const copyToClipboard = (text: string) => {
+  return copyExecCommand(text);
+};
