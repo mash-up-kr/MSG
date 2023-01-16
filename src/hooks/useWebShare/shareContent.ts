9725e668d139ad2c3032a1c1cb4d3ea1d5daf82e
HaJunRyu <hjr4557@naver.com>
2023-01-16 23:18:43 +0900

    Fix: WebShare시 NowAllowed 에러가 트리거되면 reload하게 한다

--- src/hooks/useWebShare/shareContent.ts
+++ src/hooks/useWebShare/shareContent.ts
@@ -1,5 +1,13 @@
-export const shareContent = (onSuccess: () => void, onError: () => void) => {
+export const shareContent = (onSuccess: () => void, onError: (error?: unknown) => void) => {
   return ({ files, text, title, url }: ShareData = {}) => {
-    navigator.share({ files, text, title, url }).then(onSuccess).catch(onError);
+    navigator
+      .share({ files, text, title, url })
+      .then(onSuccess)
+      .catch((error) => {
+        if (error.name === 'NotAllowedError') {
+          window.location.reload();
+        }
+        onError();
+      });
   };
 };
