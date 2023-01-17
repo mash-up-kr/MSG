733c5ee2f2a19dc50f36612bda38e30b806b0669
HaJunRyu <hjr4557@naver.com>
2023-01-17 14:28:23 +0900

    Refactor: vh를 트리거하는 resize 이벤트를 데스크탑 환경에서만 바인딩 한다

--- src/hooks/useDetectViewportHeight.ts
+++ src/hooks/useDetectViewportHeight.ts
@@ -1,14 +1,24 @@
+import { detectOS } from '@/utils/userAgent';
+import { throttle } from 'lodash-es';
 import { useEffect, useState } from 'react';
 
 const useDetectViewportHeight = () => {
   const [vh, setVh] = useState(0);
 
   useEffect(() => {
-    const detectViewportHeight = () => {
+    const detectViewportHeight = throttle(() => {
       setVh(window.innerHeight * 0.01);
-    };
+    }, 200);
+
+    if (!detectOS('Android') && !detectOS('iOS')) {
+      window.addEventListener('resize', detectViewportHeight);
+    }
 
     detectViewportHeight();
+
+    return () => {
+      window.removeEventListener('resize', detectViewportHeight);
+    };
   }, []);
 
   return { vh };
