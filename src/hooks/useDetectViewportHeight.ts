13af5030703297f007f7d763d6685bbc30b7478b
HaJunRyu <hjr4557@naver.com>
2023-01-17 14:17:02 +0900

    Refactor: resize시 vh를 트리거하는 동작을 제거한다
    모바일 브라우저에서 스크롤 동작시 resize 이벤트가 발생하여 사용성을 저하 시킨다고 판단되어 최초 1회만 vh를 트리거 하게끔 수정해주었습니다.

--- src/hooks/useDetectViewportHeight.ts
+++ src/hooks/useDetectViewportHeight.ts
@@ -1,21 +1,14 @@
 import { useEffect, useState } from 'react';
-import throttle from 'lodash-es/throttle';
 
 const useDetectViewportHeight = () => {
   const [vh, setVh] = useState(0);
 
   useEffect(() => {
-    const detectViewportHeight = throttle(() => {
+    const detectViewportHeight = () => {
       setVh(window.innerHeight * 0.01);
-    }, 200);
-
-    window.addEventListener('resize', detectViewportHeight);
+    };
 
     detectViewportHeight();
-
-    return () => {
-      window.removeEventListener('resize', detectViewportHeight);
-    };
   }, []);
 
   return { vh };
