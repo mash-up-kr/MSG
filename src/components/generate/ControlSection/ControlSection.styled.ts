c280fc425b06da8c87aad48a4b70ec1c9b082dfb
HaJunRyu <hjr4557@naver.com>
2023-01-17 13:52:03 +0900

    Refactor: 사용하지 않는 코드를 제거한다

--- src/components/generate/ControlSection/ControlSection.styled.ts
+++ src/components/generate/ControlSection/ControlSection.styled.ts
@@ -1,4 +1,3 @@
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const ControlSection = styled.section`
@@ -10,20 +9,4 @@ export const Categories = styled.div`
   flex-flow: row nowrap;
   gap: 1.2rem;
   justify-content: center;
-`;
-
-export const OptionList = styled.ul`
-  display: flex;
-  gap: 1.4rem;
-  justify-content: center;
-  margin-top: 2.4rem;
-`;
-
-export const OptionItem = styled.li`
-  ${({ theme }) => css`
-    padding: 1.2rem;
-    border: 0.22rem solid ${theme.color.gray700};
-    border-radius: 1.3rem;
-    cursor: pointer;
-  `}
 `;
