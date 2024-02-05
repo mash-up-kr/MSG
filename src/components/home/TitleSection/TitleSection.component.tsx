05a3cee7f763c7c9bc96f30d182275dfb63b4b41
HaJunRyu <hjr4557@naver.com>
2024-02-06 01:49:15 +0900

    Feat: index페이지 타이틀 문구를 변경한다

--- src/components/home/TitleSection/TitleSection.component.tsx
+++ src/components/home/TitleSection/TitleSection.component.tsx
@@ -3,8 +3,8 @@ import * as Styled from './TitleSection.styled';
 const HomeTitle = () => {
   return (
     <Styled.TitleSection>
-      <Styled.Description>매숑이와 함께하는</Styled.Description>
-      <Styled.Title>연봉 쭉 쭉 오르는{'\n'}내 직군 카드 만들기</Styled.Title>
+      <Styled.Description>새해는 매숑이와 함께!</Styled.Description>
+      <Styled.Title>CEO가 탐내는{'\n'}내 직군 카드 만들기</Styled.Title>
     </Styled.TitleSection>
   );
 };
