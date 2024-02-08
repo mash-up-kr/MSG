e04867c71789322fdcc299ed00e4e036a9b32e25
HaJunRyu <hjr4557@naver.com>
2024-02-09 02:21:30 +0900

    Chore: 눈 내리는 로띠를 임시 제거한다

--- src/components/home/BackgroundSection/BackgroundSection.component.tsx
+++ src/components/home/BackgroundSection/BackgroundSection.component.tsx
@@ -1,5 +1,4 @@
 import { LinearGradientSphere } from '@/components/common';
-import fallingSnowLottie from '@/assets/lottie/falling-snow.json';
 import * as Styled from './BackgroundSection.styled';
 
 const BackgroundSection = () => {
@@ -11,8 +10,6 @@ const BackgroundSection = () => {
           <LinearGradientSphere />
         </Styled.SphereContainer>
       </Styled.SphereDeem>
-
-      <Styled.SnowLottie animationData={fallingSnowLottie} />
     </section>
   );
 };
